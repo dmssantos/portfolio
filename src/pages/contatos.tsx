0dcc3c8df3f08ed0c770c82cc20d96fbef8f1ca7
dmssantos <dms.santos_@hotmail.com>
2023-07-31 14:51:16 -0300

    contacts page styling

--- src/pages/contatos.tsx
+++ src/pages/contatos.tsx
@@ -6,34 +6,42 @@ const Contatos = () => {
       <Head>
         <title>Contatos | David</title>
       </Head>
-      <div>
-        <h1>Contatos</h1>
-        <ul>
-          <li>
-            <span>E-mail</span>
-            <div>
-              <a href="mailto:dms.santos_@hotmail.com">dms.santos_@hotmail.com</a>
+      <div className="mt-12 md:mt-24 space-y-8 md:space-y-16 px-6 md:px-32 ">
+        <h1 className="text-5xl md:text-7xl font-bold text-center">
+          Contatos
+        </h1>
+        <ul className="table mx-auto space-y-6 md:space-y-8">
+          <li className="md:text-xl">
+            <span className="font-bold">E-mail</span>
+            <div className="flex gap-1 md:gap-3 items-center">
+              <a href="mailto:dms.santos_@hotmail.com" className="text-sm md:text-lg text-slate-300 underline truncate">dms.santos_@hotmail.com</a>
             </div>
           </li>
 
-          <li>
-            <span>Linkedin</span>
-            <div>
-              <a href="https://www.linkedin.com/in/davidsantos">https://www.linkedin.com/in/davidsantos</a>
+          <li className="md:text-xl">
+            <span className="font-bold">Linkedin</span>
+            <div className="flex gap-1 md:gap-3 items-center">
+              <a href="https://www.linkedin.com/in/davidsantos" className="text-sm md:text-lg text-slate-300 underline truncate">
+                https://www.linkedin.com/in/davidsantos
+              </a>
             </div>
           </li>
 
-          <li>
-            <span>Github</span>
-            <div>
-              <a href="https://www.github.com/dmssantos">https://www.github.com/dmssantos</a>
+          <li className="md:text-xl">
+            <span className="font-bold">Github</span>
+            <div className="flex gap-1 md:gap-3 items-center">
+              <a href="https://www.github.com/dmssantos" className="text-sm md:text-lg text-slate-300 underline truncate">
+                https://www.github.com/dmssantos
+              </a>
             </div>
           </li>
 
-          <li>
-            <span>youtube</span>
-            <div>
-              <a href="https://www.youtube.com/dmssantos">https://www.youtube.com/dmssantos</a>
+          <li className="md:text-xl">
+            <span className="font-bold">youtube</span>
+            <div className="flex gap-1 md:gap-3 items-center">
+              <a href="https://www.youtube.com/dmssantos" className="text-sm md:text-lg text-slate-300 underline truncate">
+                https://www.youtube.com/dmssantos
+              </a>
             </div>
           </li>
         </ul>
