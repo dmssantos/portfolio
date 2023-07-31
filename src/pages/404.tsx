cc9ee6f9a3bfbd3b5272526489652911d1bdb6f0
dmssantos <dms.santos_@hotmail.com>
2023-07-31 15:17:57 -0300

    404 page update  and aboutme page modded

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -13,13 +13,13 @@ const NotFound = () => {
       <Head>
         <title>404 | David</title>
       </Head>
-      <div className="flex flex-col items-center text-center mt-12 md:mt-24 gap-8 md:gap-4 px-6 md:px-32">
+      <div className="flex flex-col items-center text-center mt-12 md:mt-24 gap-8 px-6 md:px-32">
         <h1 className="text-5xl sm:text-7xl font-bold">404</h1>
-        <p className="flex flex-col gap-8 md:gap-4">
+        <p className="flex flex-col gap-8 md:gap-4 md:text-xl">
           <span>Oops, não conseguimos encontrar essa página</span>
           <span>Clique no botão abaixo para para ser redirecionado para a págian inicial</span>
         </p>
-        <Link href="/" className={`${roboto.className} p-4bg-h-blue-500 rounded-xl text-black mt-5 md:mt-12 w-fit`}>Ir para a página inicial</Link>
+        <Link href="/" className={`${roboto.className} p-4 bg-h-blue-500 rounded-xl text-black mt-5 md:mt-12 md:text-xl w-fit`}>Ir para a página inicial</Link>
       </div>
     </>
   )
