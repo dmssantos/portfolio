315f3974288c9576757222faa6c7ef4fbb56d4b7
dmssantos <dms.santos_@hotmail.com>
2023-07-31 15:05:54 -0300

    404 page styling

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,20 +1,26 @@
 import Head from "next/head"
 import Link from "next/link"
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: '500',
+});
 
 const NotFound = () => {
   return (
     <>
-    <Head>
-      <title>404 | David</title>
-    </Head>
-    <div>
-      <h1>404</h1>
-      <div>
-        <p>Oops, não conseguimos encontrar essa página</p>
-        <span>Clique no botão abaixo para para ser redirecionado para a págian inicial</span>
+      <Head>
+        <title>404 | David</title>
+      </Head>
+      <div className="flex flex-col items-center text-center mt-12 md:mt-24 gap-8 md:gap-4 px-6 md:px-32">
+        <h1 className="text-5xl sm:text-7xl font-bold">404</h1>
+        <p className="flex flex-col gap-8 md:gap-4">
+          <span>Oops, não conseguimos encontrar essa página</span>
+          <span>Clique no botão abaixo para para ser redirecionado para a págian inicial</span>
+        </p>
+        <Link href="/" className={`${roboto.className} p-4bg-h-blue-500 rounded-xl text-black mt-5 md:mt-12 w-fit`}>Ir para a página inicial</Link>
       </div>
-      <Link href="/">Ir para a página inicial</Link>
-    </div>
     </>
   )
 }
