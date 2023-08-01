2048aabe044ee8e8ce60d296d0e4c8de2a0c19b2
dmssantos <dms.santos_@hotmail.com>
2023-08-01 11:08:17 -0300

    update menu close

--- src/components/commons/Header/Menu.tsx
+++ src/components/commons/Header/Menu.tsx
@@ -9,17 +9,23 @@ interface MenuProps {
 
 export const Menu = ({ isVisible, onClose }: MenuProps) => {
   return (
-    <div className={`${isVisible ? 'flex' : 'hidden'} fixed inset-0 w-full h-full bg-black bg-opacity-40 backdrop-blur-sm md:hidden`}>
-      <div className="w-full bg-h-blue-900 h-96 shadow-md py-4 px-5">
-        <div>
+    <div
+      className={`${isVisible ? 'flex' : 'hidden'} fixed inset-0 w-full h-full bg-black bg-opacity-40 backdrop-blur-sm md:hidden`}
+      onClick={onClose}
+    >
+      <div
+        className="w-full bg-h-blue-900 h-96 shadow-md py-4 px-5"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="flex justify-between mb-5">
           <Link href="/">
-            <Image src="/favicon.svg" width={55} height={55} alt="Logo" />
+            <Image src="/favicon.ico" width={55} height={55} alt="Logo" />
           </Link>
           <button onClick={onClose}>
-            <MenuCloseIcon />
+            <MenuCloseIcon className="fill-white w-10 h-10" />
           </button>
         </div>
-        <nav>
+        <nav className="flex flex-col gap-5 text-xl p-5 items-centers">
           <Link href="/">Sobre mim</Link>
           {/* <Link href="/portfolio">Portfolio</Link> */}
           <Link href="/contatos">Entre em contato</Link>
